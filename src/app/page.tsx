7c9e7c3d6964303e079324378eb8f3ab64e1c3de
Behnam <behnamborhani95@gmail.com>
2024-02-11 00:35:53 +0330

    home features section created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { HomeHeroSection } from "./_components/home-hero-section/home-hero-section";
 import { CourseSummary } from "@/types/course-summary.interface";
 import { CourseCardList } from "./(courses)/_components/courseCardList";
+import { homeFeatures } from "@/data/home-features";
+import Feature from "./_components/feature/feature";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
@@ -15,6 +17,14 @@ export default async function Home() {
   return (
     <>
       <HomeHeroSection />
+      <section className="dark:bg-base-75 mt-10">
+        <div className="container py-10 flex flex-col lg:flex-row gap-10 xl:gap-5">
+          {homeFeatures.map((feature) => (
+            <Feature key={`feature-${feature.title}`} feature={feature} />
+          ))}
+        </div>
+      </section>
+
       <section className="container pt-20">
         <div className="text-center xl:text-right">
           <h2 className="text-2xl font-extrabold">تازه ترین دوره های آموزشی</h2>
