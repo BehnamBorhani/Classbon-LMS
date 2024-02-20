ec6b92936122cead63bd7664ee58499c18dffe43
Behnam <behnamborhani95@gmail.com>
2024-02-20 11:26:31 +0330

    useImperativeHandle to expose the ref of inputs to outside of the auth-code component

--- src/app/(auth)/verify/_components/verification-form.tsx
+++ src/app/(auth)/verify/_components/verification-form.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { AuthCode } from "@/app/_components/auth-code";
+import { AuthCodeRef } from "@/app/_components/auth-code/auth-code.types";
 import { Button } from "@/app/_components/button";
 import Link from "next/link";
+import { useRef } from "react";
 
 const VerificationForm = () => {
+  const authCodeRef = useRef<AuthCodeRef>(null);
+
   return (
     <>
       <h5 className="text-2xl">کد تایید</h5>
@@ -11,12 +15,15 @@ const VerificationForm = () => {
       <form className="flex flex-col gap-6 mt-10 flex-1">
         <AuthCode
           className="mt-10"
+          ref={authCodeRef}
           onChange={(value) => {
             console.log(value);
           }}
         />
         Timer
-        <Button isLink={true}>ارسال مجدد کد تایید</Button>
+        <Button isLink={true} onClick={authCodeRef.current?.clear}>
+          ارسال مجدد کد تایید
+        </Button>
         <Button type="submit" variant="primary">
           تایید و ادامه
         </Button>
--- src/app/_components/auth-code/auth-code.tsx
+++ src/app/_components/auth-code/auth-code.tsx
@@ -1,101 +1,127 @@
 "use client";
-import { useEffect, useRef } from "react";
-import { AuthCodeProps, AuthInputProps } from "./auth-code.types";
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+import { AuthCodeProps, AuthCodeRef, AuthInputProps } from "./auth-code.types";
 import classNames from "classnames";
 
-export const AuthCode: React.FC<AuthCodeProps> = ({
-  variant = "ghost",
-  autoFocus = true,
-  className,
-  isDisabled,
-  length = 5,
-  onChange,
-}) => {
-  if (length < 1) {
-    throw new Error("تعداد ارقام باید بزرگتر از صفر باشد!");
-  }
-
-  const inputRef = useRef<Array<HTMLInputElement>>([]);
-
-  const inputProps: AuthInputProps = {
-    min: "0",
-    max: "9",
-    pattern: "[0-9]{1}",
-  };
-
-  useEffect(() => {
-    if (autoFocus) {
-      inputRef.current[0].focus();
+// eslint-disable-next-line react/display-name
+export const AuthCode = forwardRef<AuthCodeRef, AuthCodeProps>(
+  (
+    {
+      variant = "ghost",
+      autoFocus = true,
+      className,
+      isDisabled,
+      length = 5,
+      onChange,
+    },
+    ref
+  ) => {
+    if (length < 1) {
+      throw new Error("تعداد ارقام باید بزرگتر از صفر باشد!");
     }
-  }, [autoFocus]);
 
-  const sendResult = () => {
-    const result = inputRef.current.map((input) => input.value).join("");
-    onChange(result);
-  };
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value, nextElementSibling },
-    } = e;
+    const inputRef = useRef<Array<HTMLInputElement>>([]);
 
-    if (value.match(inputProps.pattern)) {
-      if (nextElementSibling !== null) {
-        (nextElementSibling as HTMLInputElement).focus();
+    const inputProps: AuthInputProps = {
+      min: "0",
+      max: "9",
+      pattern: "[0-9]{1}",
+    };
+
+    useEffect(() => {
+      if (autoFocus) {
+        inputRef.current[0].focus();
       }
-    } else {
-      e.target.value = "";
-    }
+    }, [autoFocus]);
 
-    sendResult();
-  };
-  const handleOnFocus = (e: React.FocusEvent<HTMLInputElement>) => {
-    e.target.select();
-  };
-  const handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    const { key } = e;
-    const target = e.target as HTMLInputElement;
-    if (key === "Backspace") {
-      if (target.value === "") {
-        if (target.previousElementSibling !== null) {
-          const previousElement =
-            target.previousElementSibling as HTMLInputElement;
-          previousElement.value = "";
-          previousElement.focus();
+    const sendResult = () => {
+      const result = inputRef.current.map((input) => input.value).join("");
+      onChange(result);
+    };
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const {
+        target: { value, nextElementSibling },
+      } = e;
+
+      if (value.match(inputProps.pattern)) {
+        if (nextElementSibling !== null) {
+          (nextElementSibling as HTMLInputElement).focus();
         }
       } else {
-        target.value = "";
+        e.target.value = "";
       }
+
+      sendResult();
+    };
+    const handleOnFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+      e.target.select();
+    };
+    const handleOnKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      const { key } = e;
+      const target = e.target as HTMLInputElement;
+      if (key === "Backspace") {
+        if (target.value === "") {
+          if (target.previousElementSibling !== null) {
+            const previousElement =
+              target.previousElementSibling as HTMLInputElement;
+            previousElement.value = "";
+            previousElement.focus();
+          }
+        } else {
+          target.value = "";
+        }
+      }
+
+      sendResult();
+    };
+
+    useImperativeHandle(ref, () => ({
+      focus: () => {
+        if (inputRef.current) {
+          inputRef.current[0].focus();
+        }
+      },
+      clear: () => {
+        if (inputRef.current) {
+          for (let i = 0; i < inputRef.current.length; i++) {
+            inputRef.current[i].value = "";
+          }
+          inputRef.current[0].focus();
+        }
+
+        sendResult();
+      },
+    }));
+
+    const classes = classNames("textbox flex-1 w-1 text-center", {
+      [`textbox-${variant}`]: variant,
+    });
+
+    const inputs = [];
+    for (let i = 0; i < length; i++) {
+      inputs.push(
+        <input
+          type="text"
+          maxLength={1}
+          className={classes}
+          disabled={isDisabled}
+          onChange={handleOnChange}
+          onFocus={handleOnFocus}
+          onKeyDown={handleOnKeyDown}
+          ref={(element: HTMLInputElement) => {
+            inputRef.current[i] = element;
+          }}
+          key={`authInputCode-${i}`}
+        />
+      );
     }
 
-    sendResult();
-  };
-
-  const classes = classNames("textbox flex-1 w-1 text-center", {
-    [`textbox-${variant}`]: variant,
-  });
-
-  const inputs = [];
-  for (let i = 0; i < length; i++) {
-    inputs.push(
-      <input
-        type="text"
-        maxLength={1}
-        className={classes}
-        disabled={isDisabled}
-        onChange={handleOnChange}
-        onFocus={handleOnFocus}
-        onKeyDown={handleOnKeyDown}
-        ref={(element: HTMLInputElement) => {
-          inputRef.current[i] = element;
-        }}
-        key={`authInputCode-${i}`}
-      />
+    return (
+      <>
+        <div className={`flex gap-4 flex-row-reverse ${className}`}>
+          {inputs}
+        </div>
+      </>
     );
   }
-
-  return (
-    <>
-      <div className={`flex gap-4 flex-row-reverse ${className}`}>{inputs}</div>
-    </>
-  );
-};
+);
